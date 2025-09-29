2527a3119a1ed31b7708c3bc98bbcfd1b1c4cbf0
Deividas Martinkus <147333334+mdeividas19@users.noreply.github.com>
2025-09-29 21:37:08 +0300

    Ištrintas dashboard iš navigacijos meniu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,11 +16,6 @@
                         {{ __('Pradžia') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
             </div>
 
             <!-- Settings Dropdown -->
