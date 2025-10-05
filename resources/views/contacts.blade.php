a99c22a88ad02a31593b004ae5f7a789b92b3be1
Jonas Būbelis <jogrand07@gmail.com>
2025-10-05 14:52:23 +0300

    Create contacts.blade.php

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,37 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h3 class= "text-center text-lg font-medium mb-3">Sveiki atvykę į kontaktų puslapį!</h3>
+                </div>
+
+                <hr style="border: 2px solid white;">
+
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="flex gap-4">
+                    <p style="display: flex; align-items: center; padding: 5px 20px;">Puslapio atstovų kontaktai:</p>
+
+                    <p style="display: flex; align-items: center; border: 2px solid white;margin-left: 15px; border-radius: 15px; padding: 5px 20px;">
+                        <span>Numeris</span>
+                        <span style="border-left: 2px solid white; height: 100%; display: inline-block; margin: 0 10px;"></span>
+                        <span>+37069895616</span>
+                    </p>
+                    <p style="display: flex; align-items: center; border: 2px solid white;margin-left: 15px; border-radius: 15px; padding: 5px 20px;">
+                        <span>Email</span>
+                        <span style="border-left: 2px solid white; height: 100%; display: inline-block; margin: 0 10px;"></span>
+                        <span>info@aplinklietuva.lt</span>
+                    </p>
+                    <p style="display: flex; align-items: center; border: 2px solid white;margin-left: 15px; border-radius: 15px; padding: 5px 20px;">
+                        <span>Adresas</span>
+                        <span style="border-left: 2px solid white; height: 100%; display: inline-block; margin: 0 10px;"></span>
+                        <span>Herkaus Manto g. 84</span>
+                    </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
