bef8b8be53302716fba374d9cc061cb087b734b5
Lukas Jakavicius <lukasjakavicius9113@gmail.com>
2025-10-08 18:52:47 +0300

    apie puslapis

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,9 +4,11 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h3 class="text-lg font-medium mb-4">Sveiki atvykę!</h3>
-                    <p>Puslapis šiuo metu yra tvarkomas.</p>
+		    <p>"Aplink Lietuvą" yra projektas sukurtas padėti žmonėms pažinti Lietuvos kraštą. Čia galite sužinoti daugiau apie Lietuvos miestus, lankytinas vietoves, skaityti bei rašyti istorijas, atsiliepimus ir padėti kitiem lankytojam surasti geriausias vietas poilsiauti, pavalgyti, ar tiesiog pamatyti kažką įdomaus.</p>
+		    <img src="/img/imgApie.jpg" class="mt-8 sm:rounded-lg"></img>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
