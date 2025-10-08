2edf1d305c81f403e3dd7dc5fa194b8318d58f2a
Deividas Martinkus <147333334+mdeividas19@users.noreply.github.com>
2025-10-08 09:46:01 +0300

    Pridėtas žemėlapis

--- resources/views/cities/show.blade.php
+++ resources/views/cities/show.blade.php
@@ -22,8 +22,20 @@
                         </div>
                         @endif
                     </div>
+                    <div style="margin-top: 2rem;">
+                        <h2 style="font-size: 1.5rem; font-weight: 600; margin-bottom: 1rem;">Žemėlapis</h2>
+                        <div id="map" style="width: 100%; height: 400px; border-radius: 0.5rem;"></div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <script>
+        var map = L.map('map').setView([{{ $city->latitude ?? 55.1735998 }}, {{ $city->longitude ?? 23.8948016 }}], 13);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors', maxZoom: 19 }).addTo(map);
+        var marker = L.marker([{{ $city->latitude ?? 55.1735998 }}, {{ $city->longitude ?? 23.8948016 }}]).addTo(map);
+        marker.bindPopup("<b>{{ $city->name }}</b>");
+    </script>
 </x-app-layout>
