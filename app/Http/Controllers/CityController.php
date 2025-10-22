2ed789bf0379f423f069ad6f06b913685e175154
Deividas Martinkus <147333334+mdeividas19@users.noreply.github.com>
2025-10-22 11:06:50 +0300

    Vietų peržiūra miesto žemėlapyje

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\City;
+use App\Models\Locations;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
@@ -17,7 +18,23 @@ class CityController extends Controller
     {
         $city = City::findOrFail($id);
         
-        return view('cities.show', compact('city'));
+        $locations = Locations::where('city_id', $id)
+            ->with(['images' => function($query) {
+                $query->select('location_id', 'image_path')->limit(1);
+            }])
+            ->select('id', 'name', 'latitude', 'longitude')
+            ->get()
+            ->map(function($location) {
+                return [
+                    'id' => $location->id,
+                    'name' => $location->name,
+                    'latitude' => $location->latitude,
+                    'longitude' => $location->longitude,
+                    'image_path' => $location->images->first()->image_path ?? null
+                ];
+            });
+
+        return view('cities.show', compact('city', 'locations'));
     }
 
 }
--- resources/views/cities/show.blade.php
+++ resources/views/cities/show.blade.php
@@ -33,9 +33,24 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     <script>
-        var map = L.map('map').setView([{{ $city->latitude ?? 55.1735998 }}, {{ $city->longitude ?? 23.8948016 }}], 13);
+        var map = L.map('map').setView([{{ $city->latitude ?? 55.1735998 }}, {{ $city->longitude ?? 23.8948016 }}], 11);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors', maxZoom: 19 }).addTo(map);
         var marker = L.marker([{{ $city->latitude ?? 55.1735998 }}, {{ $city->longitude ?? 23.8948016 }}]).addTo(map);
         marker.bindPopup("<b>{{ $city->name }}</b>");
+        var locations = @json($locations);
+        locations.forEach(function(location) {
+            var marker = L.marker([location.latitude, location.longitude]).addTo(map);
+            var popupContent = '<div style="text-align: center;">';
+            if (location.image_path) {
+                popupContent += '<img src="/storage/location_images/' + location.image_path + '" alt="' + location.name + '" style="max-width: 150px; max-height: 100px; width: auto; height: auto; object-fit: contain; border-radius: 0.25rem; margin-bottom: 0.5rem; display: block; margin-left: auto; margin-right: auto;">';
+            }
+            popupContent += '<b style="display: block; margin-bottom: 0.25rem;">' + location.name + '</b>' +
+                           '<a href="/locations/' + location.id + '" style="color: #3b82f6; text-decoration: underline;">Apie</a>' +
+                           '</div>';
+            marker.bindPopup(popupContent, {
+                maxWidth: 200,
+                minWidth: 170
+            });
+        });
     </script>
 </x-app-layout>
