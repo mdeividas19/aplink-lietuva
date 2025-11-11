1167497d8ab0313e20c84968dd83b5095d68d2aa
wick66d <gustasgre@gmail.com>
2025-11-11 21:56:13 +0200

    Smulkus pakeitimai WIP

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-semibold text-sm text-gray-800']) }}>
+<label {{ $attributes->merge(['class' => 'block font-semibold text-sm text-black']) }}>
     {{ $value ?? $slot }}
 </label>
