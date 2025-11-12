d0c841feead90908ded92ef1c254677a0f3d0cd6
Lukas Jakavicius <lukasjakavicius9113@gmail.com>
2025-11-12 11:21:11 +0200

    sutaisytas admin dashboardo editUser

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use app\Models\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
@@ -21,12 +21,18 @@ class AdminController extends Controller
 	}
 
     public function editUser(User $user){
-	    #dd($user);  
+	   # dd($user);  
 	 abort_if(!$user->exists, 404);  
 	return view('admin.editUser', compact('user'));
     }
 
     public function updateUser(Request $request, User $user){
-	return 0;
+    $request->validate([
+	'name' => 'required|string|max:255',
+	'role' => 'required|integer',
+    ]);
+    $user->update($request->only(['name', 'role']));
+    #return view('admin.adminDashboard');
+    return redirect(route('admin.dashboard'));
     }
 }
